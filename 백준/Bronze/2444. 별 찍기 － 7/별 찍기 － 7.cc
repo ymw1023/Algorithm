3efd2fce18f8f96a3344847dbcd575bd763b9be9
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-03-25 21:04:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*i+1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<i+1;j++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*n-2*i-3;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
