763c6a30f287b8f846dadb97327d8248ae341ec2
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-04-04 23:23:21 +0900

    [Silver II] Title: 나무 자르기, Time: 1944 ms, Memory: 320696 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();       //나무의 수     max = 1,000,000
+        int num = sc.nextInt();     //필요한 길이    max = 2,000,000,000
+                                    //나무길이      max = 1,000,000,000
+        int[] arr = new int[n + 1];     //나무의 길이를 담을 배열
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();  //나무의 길이를 배열에 담음
+        }
+
+        Arrays.sort(arr);       //오름차순 정렬
+        int i = 1;      //잘리는 나무의 갯수 - 톱이 위에서 내려오는 느낌
+        int sum = 0;    //현재 자른 나무 길이
+        while(true) {
+            long cnt = (long)(arr[arr.length - i] - arr[arr.length - i - 1]) * i;  //(현재 나무길이 - 다음 나무길이) * 현재 나무 수
+            if(sum + cnt >= num) {  //넘치면 break;
+                break;
+            } else {        //안 넘치면 더해줌
+                sum += cnt;
+            }
+            i++;        //나무 한개 추가
+        }
+                //현재 나무 길이      -    남은 필요 나무 길이 / 현재 나무 수
+        int answer = arr[arr.length - i] - (int)Math.ceil((double)(num - sum) / i);
+
+        System.out.println(answer);
+    }
+}
