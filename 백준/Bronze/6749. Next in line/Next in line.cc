dcae6caa8519056ccb833bcc2d6075b436f88ec0
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-04-11 23:27:01 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int first,mid,yc;
+    
+    scanf("%d",&yc);
+    scanf("%d",&mid);
+    
+    first=mid*2-yc;
+    
+    printf("%d",first);
+    
+    return 0;
+}
