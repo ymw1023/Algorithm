9fc4df9c1640152e1aa69f48224b9149353801e8
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-04-14 23:42:00 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        printf("Hello World, Judge %d!\n",i+1);
+    }
+
+    return 0;
+}
