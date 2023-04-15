a4f98764d3f8fd4d128e63201e650449c343220d
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-04-15 23:27:49 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int A;
+   
+   scanf("%d",&A);
+   
+   switch(A/10)
+   {
+       case 10:
+       case 9:
+           printf("A");
+           break;
+       
+       case 8:
+           printf("B");
+           break;
+       case 7:
+           printf("C");
+           break;
+       case 6:
+           printf("D");
+           break;
+       default:
+           printf("F");
+           break;
+   }
+   
+   
+    return 0;
+    
+}
