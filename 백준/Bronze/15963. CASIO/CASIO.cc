e7f76e71b1e56b48afcb2903a0ba7b5907ebf6f2
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-05-08 23:38:03 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n,m;
+    
+    scanf("%lld %lld",&n,&m);
+    
+    if(n==m)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    
+    return 0;
+}
