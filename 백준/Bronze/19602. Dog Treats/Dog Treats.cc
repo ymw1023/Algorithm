80b2f346f7d558e8b7159380b9b6075945d397d2
ymw1023 <119826518+ymw1023@users.noreply.github.com>
2023-05-19 23:47:31 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int S,M,L;
+    
+    scanf("%d",&S);
+    scanf("%d",&M);
+    scanf("%d",&L);
+    
+    printf("%s",1*S+2*M+3*L>=10?"happy":"sad");
+    
+    return 0;
+}
